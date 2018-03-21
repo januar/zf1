0ab2c562c8302d9e271aa7467802ecfac96152c9
Januar Sirait <januar.srt@gmail.com>
2018-03-21 18:39:01 +0700

    config only for php7.1

--- tests/runalltests.php
+++ tests/runalltests.php
@@ -64,10 +64,12 @@ while(list(, $file)=each($files)) {
         continue;
     }
 
+    echo "================================ START ================================" . PHP_EOL;
     echo "Executing {$file}" . PHP_EOL;
     system($PHPUNIT . ' --stderr -d memory_limit=-1 -d error_reporting=E_ALL\&E_STRICT -d display_errors=1 ' . escapeshellarg($file), $c_result);
     echo PHP_EOL;
     echo "Finished executing {$file}" . PHP_EOL;
+    echo "================================ END ================================" . PHP_EOL;
     
     if ($c_result) {
         echo PHP_EOL . "Result of $file is $c_result" . PHP_EOL . PHP_EOL;
